1a6f98f6cc384ceb48fb1d4d7c71350c964b97e7
JonathanAL003 <jonathan_alonsolara@yahoo.com>
2022-11-27 22:09:49 -0600

    Version 3.7 Grid con conexion a BDmySQL

---
+++ Parcial3/Grid/jsGrid.js
@@ -0,0 +1,58 @@
+$(document).ready(function(){
+ 
+    var categorias = [
+        { Name: "", Id: 0 },
+        { Name: "A la parilla", Id: 1 },
+        { Name: "Bebidas y cafe", Id: 2 },
+        { Name: "Complementos", Id: 3 },
+        { Name: "De Pollo", Id:4},
+        { Name: "Desayunos", Id:5},
+        { Name: "Ensaladas", Id:6},
+        { Name: "Niños", Id:7},
+        { Name: "Postres", Id:8}
+    ];
+
+    const settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "http://localhost/Alonso_ProgramacionWeb/Parcial3/Grid/Consulta.php",
+        method: "GET",
+        "headers": {
+            "Accept": ""/""
+        }
+    };
+ 
+    $.ajax(settings).done(function(response){
+        console.log(response);
+        let tabla = JSON.parse(response);
+        console.log(tabla);
+    
+
+
+    $("#jsGrid").jsGrid({
+        width: "100%",
+        height: "400px",
+ 
+        inserting: true,
+        editing: true,
+        sorting: true,
+        paging: true,
+ 
+        data: tabla,
+
+        fields: [
+            { name: "Id_Producto", type: "number", width: 30, validate: "required" },
+            { name: "Nombre", type: "text", width: 100 },
+            { name: "Categoria", type: "text", with:100},
+            { name: "Descripcion", type: "text", with: 100 },
+            { name: "Precio", type: "number", width: 50 },
+            { name: "Imagen", type: "text", width: 100 },
+            { name: "Valoracion", type: "number", width: 50 },
+            { name: "Ingredientes", type: "text", width: 100 },
+            { name: "Costo", type: "number", width: 50 },
+            { name: "Minutos", type: "number", width: 50 },
+            { type: "control" }
+            ]
+        });
+    });   
+});
