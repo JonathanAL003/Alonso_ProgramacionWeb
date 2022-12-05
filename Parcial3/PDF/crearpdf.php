78e2d8db9cccd88becaaa3f5f4f0f8784abaa610
JonathanAL003 <jonathan_alonsolara@yahoo.com>
2022-12-04 20:51:21 -0600

    Version 3.10.1 Generar PDF

--- Parcial3/PDF/crearpdf.php
+++ Parcial3/PDF/crearpdf.php
@@ -1,12 +1,23 @@
 <?php
-
-$nombre = $_POST['nombre'];
-
 require('./FPDF/fpdf.php');
 
+$nombre = $_POST['nombre'];
+$telefono = $_POST['telefono'];
+
 $pdf = new FPDF();
+$pdf->SetMargins(17, 17, 17);
 $pdf->AddPage();
-$pdf->SetFont('Arial','B',16);
-$pdf->Cell(40,10,'¡Hola, Mundo!' .$nombre);
+
+
+$pdf->SetFont('Arial','',10);
+$pdf->SetTextColor(39,39,51);
+$pdf->Cell(13,7,'Nombre:');
+$pdf->Cell(60,7,' '.$nombre);
+$pdf->Ln(5);
+$pdf->Cell(15,7,'Telefono:');
+$pdf->Cell(60,7,' '.$telefono);
+
+
+
 $pdf->Output();
 ?>
